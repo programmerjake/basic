d18d78506e58833f911d986ffb7e26765ca96570
programmerjake <programmerjake@gmail.com>
2015-08-09 11:45:25 -0700

    created example webpage

--- README.md
+++
@@ -1,6 +0,0 @@
-basic
-=====
-
-basic dialect
-
-to build the project you have to have [parser-generator](https://github.com/programmerjake/parser-generator/) installed
--- emscripten_compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-opt_arg="-O3"
-if [ "$1" == "--debug" -o "$1" == "-d" ]; then
-    opt_arg="-O1"
-fi
-echo em++ -s DISABLE_EXCEPTION_CATCHING=0 -s ALLOW_MEMORY_GROWTH=1 $opt_arg -Wall -std=c++11 -o bin/basic.html -I. basic.parser.cpp code_writer_c.cpp code_writer_dump.cpp main.cpp ast/*.cpp
-em++ -s DISABLE_EXCEPTION_CATCHING=0 -s ALLOW_MEMORY_GROWTH=1 $opt_arg -Wall -std=c++11 -o bin/basic.html -I. basic.parser.cpp code_writer_c.cpp code_writer_dump.cpp main.cpp ast/*.cpp
